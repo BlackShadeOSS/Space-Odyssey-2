64582376014cbfe4d3c58f28cb6c981ab037a45a
BlackShade <diaxsio10@gmail.com>
2023-05-14 22:07:26 +0200

    Added information and profile page, added endless table and some fixes

--- JS/tableGeneration.js
+++ JS/tableGeneration.js
@@ -1,5 +1,6 @@
 // use xhr to dowload data in json format form server and generate table in html
-
+const scoreboard = document.querySelector(".scoreboard");
+const buttons = document.querySelectorAll(".switchButton");
 // get data from server
 function getData() {
     var xhr = new XMLHttpRequest();
@@ -8,6 +9,32 @@ function getData() {
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
             var data = Object.values(JSON.parse(xhr.responseText));
+            var level6data = [];
+            //get data from level 6 and push it to new array and remove it from old array
+            data.forEach(function (rowData) {
+                if (rowData.level == 6) {
+                    level6data.push(rowData);
+                    data.splice(data.indexOf(rowData), 1);
+                }
+            });
+            //sort data by time
+            level6data.sort(function (a, b) {
+                return a.time - b.time;
+            });
+
+            level6data.forEach(function (
+                rowData,
+                index = data.indexOf(rowData)
+            ) {
+                rowData.id = index + 1;
+                delete rowData.level;
+
+                //change miliseconds to minutes and seconds
+                var time = new Date(rowData.time);
+                var minutes = time.getMinutes().toString().padStart(2, "0");
+                var seconds = time.getSeconds().toString().padStart(2, "0");
+                rowData.time = minutes + ":" + seconds;
+            });
             //change id to place in table
             data.forEach(function (rowData, index = data.indexOf(rowData)) {
                 rowData.id = index + 1;
@@ -18,33 +45,85 @@ function getData() {
                 var seconds = time.getSeconds().toString().padStart(2, "0");
                 rowData.time = minutes + ":" + seconds;
             });
-            generateTable(data);
+            generateTable(data, "table1-5");
+            generateTable(level6data, "table6");
         }
     };
 }
 
 // generate table in html
-function generateTable(tableData) {
-    var tableBody = document.querySelector("tbody");
-    console.log(tableData);
+function generateTable(tableData, id) {
+    var table = document.createElement("table");
+    table.innerHTML = `
+    <table>
+    <thead>
+        <tr>
+            <th>Place</th>
+            <th>Nickname</th>
+            ${id == "table1-5" ? "<th>Level</th>" : ""}
+            <th>Time</th>
+        </tr>
+    </thead>
+    </table>`;
+    table.setAttribute("id", id);
+    var tableBody = document.createElement("tbody");
 
-    tableData.forEach(function (rowData) {
-        rowData = Object.values(rowData);
-        var row = document.createElement("tr");
+    if (tableData.length == 0) {
+        table.innerHTML = `
+        <table>
+        <thead>
+            <tr>
+                <th>No Data Yet</th>
+            </tr>
+        </thead>
+        </table>`;
+    } else {
+        tableData.forEach(function (rowData) {
+            rowData = Object.values(rowData);
+            var row = document.createElement("tr");
 
-        console.log(rowData);
+            rowData.forEach(function (cellData) {
+                var cell = document.createElement("td");
+                cell.appendChild(document.createTextNode(cellData));
+                row.appendChild(cell);
+            });
 
-        rowData.forEach(function (cellData) {
-            var cell = document.createElement("td");
-            cell.appendChild(document.createTextNode(cellData));
-            row.appendChild(cell);
+            tableBody.appendChild(row);
         });
+    }
 
-        tableBody.appendChild(row);
-    });
+    if (id == "table1-5") {
+        table.style.display = "block";
+    }
+
+    table.appendChild(tableBody);
+    scoreboard.appendChild(table);
 }
 
 // generate table when page is loaded
 window.onload = function () {
     getData();
 };
+
+window.addEventListener("load", function () {
+    buttons.forEach(function (button) {
+        button.addEventListener("click", switchTable);
+    });
+});
+
+// swich between tables
+function switchTable() {
+    var table1 = document.getElementById("table1-5");
+    var table6 = document.getElementById("table6");
+    if (buttons[1].classList.contains("activeButton")) {
+        table1.style.display = "block";
+        table6.style.display = "none";
+        buttons[0].classList.toggle("activeButton");
+        buttons[1].classList.toggle("activeButton");
+    } else if (buttons[0].classList.contains("activeButton")) {
+        table1.style.display = "none";
+        table6.style.display = "block";
+        buttons[0].classList.toggle("activeButton");
+        buttons[1].classList.toggle("activeButton");
+    }
+}
